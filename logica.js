dbb3e9e8e00bf4c12b02b4e97c6c3a514ac921fa
Your Namecris-clm <you@example.comcristianleo_5@gmail.com>
2021-03-23 09:44:58 -0300

    primer commit

---
+++ logica.js
@@ -0,0 +1,35 @@
+function Enviar (){
+    
+    let Ciudad = document.getElementById('ciudad').value;
+    let Habitantes = document.getElementById('habitantes').value;
+    let Continente = document.getElementById('continente').value;
+    
+    let td1 = document.createElement('td');
+    td1.textContent = Ciudad;
+    let td2 = document.createElement('td');
+    td2.textContent = Habitantes;
+    let td3 = document.createElement('td');
+    td3.textContent = Continente;
+
+    let Tr = document.createElement('tr');
+
+    Tr.appendChild(td1);
+    Tr.appendChild(td2);
+    Tr.appendChild(td3);
+
+    let TablaPresentacion = document.getElementById('cabecera');
+    TablaPresentacion.appendChild(Tr);
+
+    
+    alert('Agregado con Exito');
+
+}
+
+function Borrar() {
+
+    alert("usted esta apunto de borrar los últimos Registros")
+
+    tr.removeChild(tr.td)
+    
+
+}
